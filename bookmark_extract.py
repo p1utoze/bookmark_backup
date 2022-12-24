03d72606007eb379c0416f234d86b12b962cf48d
p1utoze <aditya.n.awati@gmail.com>
2022-12-24 22:42:24 +0530

    Update bookmark_extract.py file

--- bookmark_extract.py
+++ bookmark_extract.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import subprocess
 import json
@@ -5,11 +6,18 @@ book_dict = dict()
 _user = subprocess.check_output("whoami", shell=True).decode().strip()
 _path = "/home/{0}/.config/".format(_user)
 _path_len = _path.split('/').__len__()
-for paths, dirs, files in os.walk(_path):
-    if paths.split('/').__len__() == _path_len and os.path.basename(paths).startswith("Brave"):
-        path = paths
-        print()
-        break
+
+
+def getpath(_path):
+    for paths, dirs, files in os.walk(_path):
+        if paths.split('/').__len__() == _path_len and os.path.basename(paths).startswith("Brave"):
+            path = paths
+            print()
+            break
+    return _path
+
+
+_path = getpath(_path)
 _cmd = 'find {0} -name "Book*s"'.format(_path)
 bookmark_file = subprocess.check_output(_cmd, shell=True).decode().strip()
 
@@ -17,3 +25,4 @@ bookmark_file = subprocess.check_output(_cmd, shell=True).decode().strip()
 def bookmark_dict():
     with open(bookmark_file, "r") as f:
         return json.load(f)
+
